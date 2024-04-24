e2c7aa1fadd5ed505d5dc0d27ff4e848c777028e
jullysilva <jkasilva@sga.pucminas.br>
2024-04-24 20:03:45 -0300

    deploy

---
+++ src/main/java/com/example/demo/Controllers/MainController.java
@@ -0,0 +1,13 @@
+package network.webtech.labspringmvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+  
+    @GetMapping("/")
+    public String index() {
+        return "Hello, World!";
+    }
+}
